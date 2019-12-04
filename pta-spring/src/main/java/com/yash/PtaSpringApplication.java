e601d3226485853453ec28a03f1f69b50aebeb8e
KazimKSyed <syedkazim56@gmail.com>
2019-12-04 10:54:59 +0530

    pta project created..!

---
+++ pta-spring/src/main/java/com/yash/PtaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.yash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PtaSpringApplication.class, args);
+	}
+
+}
